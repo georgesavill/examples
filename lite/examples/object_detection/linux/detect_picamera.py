0f1916eb4c762f4b530b5ae2f26bc4acbeb650a8
georgesavill <gpsavill+github@gmail.com>
2020-04-10 11:51:28 +0100

    Remove unneeded code

--- lite/examples/object_detection/linux/detect_picamera.py
+++ lite/examples/object_detection/linux/detect_picamera.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Example using TF Lite to detect objects with the Raspberry Pi camera."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -21,33 +20,11 @@ from __future__ import print_function
 
 import argparse
 import io
-import re
-import time
-
-from annotation import Annotator
 
 import numpy as np
-import picamera
 
 from PIL import Image
 from tflite_runtime.interpreter import Interpreter
-
-CAMERA_WIDTH = 640
-CAMERA_HEIGHT = 480
-
-
-def load_labels(path):
-  """Loads the labels file. Supports files with or without index numbers."""
-  with open(path, 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    labels = {}
-    for row_number, content in enumerate(lines):
-      pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
-      if len(pair) == 2 and pair[0].strip().isdigit():
-        labels[int(pair[0])] = pair[1].strip()
-      else:
-        labels[row_number] = pair[0].strip()
-  return labels
 
 
 def set_input_tensor(interpreter, image):
@@ -86,24 +63,6 @@ def detect_objects(interpreter, image, threshold):
       results.append(result)
   return results
 
-
-def annotate_objects(annotator, results, labels):
-  """Draws the bounding box and label for each object in the results."""
-  for obj in results:
-    # Convert the bounding box figures from relative coordinates
-    # to absolute coordinates based on the original resolution
-    ymin, xmin, ymax, xmax = obj['bounding_box']
-    xmin = int(xmin * CAMERA_WIDTH)
-    xmax = int(xmax * CAMERA_WIDTH)
-    ymin = int(ymin * CAMERA_HEIGHT)
-    ymax = int(ymax * CAMERA_HEIGHT)
-
-    # Overlay the box, label, and score on the camera preview
-    annotator.bounding_box([xmin, ymin, xmax, ymax])
-    annotator.text([xmin, ymin],
-                   '%s\n%.2f' % (labels[obj['class_id']], obj['score']))
-
-
 def main():
   parser = argparse.ArgumentParser(
       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -119,36 +78,14 @@ def main():
       default=0.4)
   args = parser.parse_args()
 
-  labels = load_labels(args.labels)
   interpreter = Interpreter(args.model)
   interpreter.allocate_tensors()
   _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
 
-  with picamera.PiCamera(
-      resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=30) as camera:
-    camera.start_preview()
-    try:
-      stream = io.BytesIO()
-      annotator = Annotator(camera)
-      for _ in camera.capture_continuous(
-          stream, format='jpeg', use_video_port=True):
-        stream.seek(0)
-        image = Image.open(stream).convert('RGB').resize(
-            (input_width, input_height), Image.ANTIALIAS)
-        start_time = time.monotonic()
-        results = detect_objects(interpreter, image, args.threshold)
-        elapsed_ms = (time.monotonic() - start_time) * 1000
+  image = Image.open("./test.jpeg").convert('RGB').resize((input_width, input_height), Image.ANTIALIAS)
 
-        annotator.clear()
-        annotate_objects(annotator, results, labels)
-        annotator.text([5, 0], '%.1fms' % (elapsed_ms))
-        annotator.update()
-
-        stream.seek(0)
-        stream.truncate()
-
-    finally:
-      camera.stop_preview()
+  results = detect_objects(interpreter, image, args.threshold)
+  print(results)
 
 
 if __name__ == '__main__':
